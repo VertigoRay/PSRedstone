348a5bb4a80a16d56a21ded187d9f1ccb9b73986
Raymond Piller <Raymond.Piller@utsa.edu>
2022-11-02 00:39:26 -0500

    $psake.build_success
    The property 
    'use_exit_on_error' cannot be found on this object. Verify that the property 
    exists and can be set.

--- .build/build.ps1
+++ .build/build.ps1
@@ -67,7 +67,8 @@ foreach ($module in $installModules.GetEnumerator()) {
 }
 
 # Run the *real* build script.
-if ($env:CI) {
-    $psake.use_exit_on_error = $true
-}
 Invoke-psake -BuildFile "$PSScriptRoot\buildPsake.ps1" -TaskList $Task -Verbose:$VerbosePreference
+
+if ($env:CI -and -not $psake.build_success) {
+    $Host.SetShouldExit(1)
+}
