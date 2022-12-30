eb3ff4265b3d2002a840cf6d6b5dc2293267514a
Raymond Piller <Raymond.Piller@utsa.edu>
2022-12-30 00:27:37 -0600

    Added more output

--- .build/buildPrep.ps1
+++ .build/buildPrep.ps1
@@ -13,7 +13,10 @@ $installModules = @{
 
 $ErrorActionPreference = 'Stop'
 trap {
-    Write-Error ('( ಥ ͜ʖ ͡ಥ) {0}' -f $_) -ErrorAction 'Continue'
+    Write-Warning ('{0}' -f $_)
+    Write-Warning ($_ | Out-String)
+    Write-Warning (Get-PSCallStack | Out-String)
+    Write-Error $_ -ErrorAction 'Continue'
     if ($env:CI) {
         $Host.SetShouldExit(1)
     }
@@ -23,17 +26,17 @@ if (Test-Path ([IO.Path]::Combine($PSScriptRoot, 'env.ps1'))) {
     . ([IO.Path]::Combine($PSScriptRoot, 'env.ps1'))
 }
 
-# Enable TLS v1.2 (for GitHub et al.)
+Write-Information 'Enable TLS v1.2 (for GitHub et al.)'
 Write-Verbose "[BUILD] SecurityProtocol OLD: $([System.Net.ServicePointManager]::SecurityProtocol)"
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor [System.Net.SecurityProtocolType]::Tls12
 Write-Verbose "[BUILD] SecurityProtocol NEW: $([System.Net.ServicePointManager]::SecurityProtocol)"
 
-# Setup NuGet PP
+Write-Information 'Setup NuGet PP'
 if (-not (Get-PackageProvider 'NuGet' -ErrorAction 'Ignore' | Where-Object { $_.Version -ge $NuGetPPMinVersion })) {
     Install-PackageProvider -Name 'NuGet' -MinimumVersion $NuGetPPMinVersion -Force
 }
 
-# Get rid of older MS Pester v3
+Write-Information 'Get rid of older MS Pester v3'
 if ((Get-Module 'Pester' -ErrorAction 'Ignore') -and ((Get-Module 'Pester' -ErrorAction 'Ignore').Version -ne $PesterVersion)) {
     foreach ($pester in (Get-Module 'Pester' -ErrorAction 'Ignore')) {
         if (([IO.DirectoryInfo] $pester.ModuleBase).Parent.Exists) {
@@ -48,24 +51,24 @@ if ((Get-Module 'Pester' -ErrorAction 'Ignore') -and ((Get-Module 'Pester' -Erro
     Remove-Module 'Pester' -Force
 }
 
-# Install all of the modules in the $installModules hashtable.
+Write-Information 'Install all of the modules in the $installModules hashtable.'
 foreach ($module in $installModules.GetEnumerator()) {
-    # if (-not (Get-Module -Name $module.Name -ListAvailable -ErrorAction 'Ignore')) {
-        if ($module.Value -is [hashtable]) {
-            $install = $module.Value
-            $install.Set_Item('Name', $module.Name)
-        } else {
-            $install = @{
-                Name = $module.Name
-            }
-            if ($module.Value -ne 'latest') {
-                $install.Set_Item('RequiredVersion', $module.Value)
-            }
+    if ($module.Value -is [hashtable]) {
+        $install = $module.Value
+        $install.Set_Item('Name', $module.Name)
+    } else {
+        $install = @{
+            Name = $module.Name
         }
-        Install-Module @install -Scope 'CurrentUser' -Force
-        Get-Module -Name $module.Name -ListAvailable | Format-Table
-    # }
+        if ($module.Value -ne 'latest') {
+            $install.Set_Item('RequiredVersion', $module.Value)
+        }
+    }
+    Install-Module @install -Scope 'CurrentUser' -Force
+
+    $m = Get-Module -Name $module.Name -ListAvailable
+    Write-Information '{0}Installed: {1} {2}' -f "`t", $m.Name, $m.Version
 }
 
-# Install all build requirements
+Write-Information 'Install all build requirements'
 Invoke-PSDepend -Path ([IO.Path]::Combine($PSScriptRoot, 'REQUIREMENTS.psd1'))
