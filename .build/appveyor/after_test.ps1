d641b514d55a16ac62db62af50e77c238e7c0f83
Raymond Piller <ray@vertigion.com>
2022-12-31 17:22:55 -0600

    Fixed lint
    no longer need this file.

--- .build/appveyor/after_test.ps1
+++
@@ -1,8 +0,0 @@
-$invokePsake = @{
-    BuildFile = [IO.Path]::Combine($env:APPVEYOR_BUILD_FOLDER, '.build', 'buildPsake.ps1')
-    TaskList = 'CodeCov'
-}
-Invoke-psake @invokePsake
-if (-not $psake.build_success) {
-    $Host.SetShouldExit(1)
-}
