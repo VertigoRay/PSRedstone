87d143c09789c244fa29b06060ddccb125563408
Raymond Piller <ray@vertigion.com>
2023-01-22 05:51:59 -0600

    Added: Get-RedstoneInstalledApplication.Tests

--- PSRedstone/Public/Get-RedstoneInstalledApplication.ps1
+++ PSRedstone/Public/Get-RedstoneInstalledApplication.ps1
@@ -26,32 +26,49 @@ Get-RedstoneInstalledApplication -ProductCode '{1AD147D0-BE0E-3D6C-AC11-64F6DC41
 .LINK
 http://psappdeploytoolkit.com
 #>
-Function Get-RedstoneInstalledApplication {
-    [CmdletBinding()]
+function Get-RedstoneInstalledApplication {
+    [CmdletBinding(DefaultParameterSetName = 'like')]
     Param (
-        [Parameter(Mandatory = $false, Position = 0)]
+        [Parameter(Mandatory = $false, Position = 0, ParameterSetName = 'eq')]
+        [Parameter(Mandatory = $false, Position = 0, ParameterSetName = 'exact')]
+        [Parameter(Mandatory = $false, Position = 0, ParameterSetName = 'like')]
+        [Parameter(Mandatory = $false, Position = 0, ParameterSetName = 'regex')]
         [ValidateNotNullorEmpty()]
         [string[]]
-        $Name,
+        $Name = '*',
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'eq')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'exact')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'like')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'regex')]
         [switch]
-        $Exact = $false,
+        $CaseSensitive,
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'exact')]
+        [switch]
+        $Exact,
+
+        [Parameter(Mandatory = $false, ParameterSetName = 'like')]
         [switch]
         $WildCard,
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'regex')]
+        [switch]
+        $RegEx,
+
+        [Parameter(Mandatory = $false, ParameterSetName = 'productcode')]
         [ValidateNotNullorEmpty()]
         [string]
         $ProductCode,
 
-        [Parameter(Mandatory = $false)]
+        [Parameter(Mandatory = $false, ParameterSetName = 'eq')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'exact')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'like')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'regex')]
+        [Parameter(Mandatory = $false, ParameterSetName = 'productcode')]
         [switch]
         $IncludeUpdatesAndHotfixes,
 
-        [Parameter(Mandatory = $false, HelpMessage="Private Parameter; used for debug overrides.")]
         [ValidateNotNullorEmpty()]
         [string[]]
         $UninstallRegKeys = @(
@@ -61,6 +78,7 @@ Function Get-RedstoneInstalledApplication {
     )
 
     Write-Information "[Get-RedstoneInstalledApplication] > $($MyInvocation.BoundParameters | ConvertTo-Json -Compress)"
+    Write-Information "[Get-RedstoneInstalledApplication] ParameterSetName> $($PSCmdlet.ParameterSetName | ConvertTo-Json -Compress)"
     Write-Debug "[Get-RedstoneInstalledApplication] Function Invocation: $($MyInvocation | Out-String)"
 
 
@@ -72,37 +90,37 @@ Function Get-RedstoneInstalledApplication {
     }
 
     ## Enumerate the installed applications from the registry for applications that have the "DisplayName" property
-    [psobject[]]$regKeyApplication = @()
+    [psobject[]] $regKeyApplication = @()
     foreach ($regKey in $UninstallRegKeys) {
         Write-Verbose "[Get-RedstoneInstalledApplication] Checking Key: ${regKey}"
         if (Test-Path -LiteralPath $regKey -ErrorAction 'SilentlyContinue' -ErrorVariable '+ErrorUninstallKeyPath') {
-            [psobject[]]$UninstallKeyApps = Get-ChildItem -LiteralPath $regKey -ErrorAction 'SilentlyContinue' -ErrorVariable '+ErrorUninstallKeyPath'
+            [psobject[]] $UninstallKeyApps = Get-ChildItem -LiteralPath $regKey -ErrorAction 'SilentlyContinue' -ErrorVariable '+ErrorUninstallKeyPath'
             foreach ($UninstallKeyApp in $UninstallKeyApps) {
                 Write-Verbose "[Get-RedstoneInstalledApplication] Checking Key: $($UninstallKeyApp.PSChildName)"
                 try {
-                    [psobject]$regKeyApplicationProps = Get-ItemProperty -LiteralPath $UninstallKeyApp.PSPath -ErrorAction 'Stop'
-                    if ($regKeyApplicationProps.DisplayName) { [psobject[]]$regKeyApplication += $regKeyApplicationProps }
+                    [psobject] $regKeyApplicationProps = Get-ItemProperty -LiteralPath $UninstallKeyApp.PSPath -ErrorAction 'Stop'
+                    if ($regKeyApplicationProps.DisplayName) { [psobject[]] $regKeyApplication += $regKeyApplicationProps }
                 } catch {
-                    Write-Warning "[Get-RedstoneInstalledApplication] Unable to enumerate properties from registry key path [$($UninstallKeyApp.PSPath)]. `n$(Resolve-Error)"
+                    Write-Warning "[Get-RedstoneInstalledApplication] Unable to enumerate properties from registry key path [$($UninstallKeyApp.PSPath)].$(if (Get-Command 'Resolve-Error' -ErrorAction 'Ignore') { "`n{0}" -f (Resolve-Error) })"
                     continue
                 }
             }
         }
     }
     if ($ErrorUninstallKeyPath) {
-        Write-Warning "[Get-RedstoneInstalledApplication] The following error(s) took place while enumerating installed applications from the registry. `n$(Resolve-Error -ErrorRecord $ErrorUninstallKeyPath)"
+        Write-Warning "[Get-RedstoneInstalledApplication] The following error(s) took place while enumerating installed applications from the registry.$(if (Get-Command 'Resolve-Error' -ErrorAction 'Ignore') { "`n{0}" -f (Resolve-Error -ErrorRecord $ErrorUninstallKeyPath) })"
     }
 
     ## Create a custom object with the desired properties for the installed applications and sanitize property details
-    [psobject[]]$installedApplication = @()
+    [psobject[]] $installedApplication = @()
     foreach ($regKeyApp in $regKeyApplication) {
         try {
-            [string]$appDisplayName = ''
-            [string]$appDisplayVersion = ''
-            [string]$appPublisher = ''
+            [string] $appDisplayName = ''
+            [string] $appDisplayVersion = ''
+            [string] $appPublisher = ''
 
             ## Bypass any updates or hotfixes
-            if (-not $IncludeUpdatesAndHotfixes) {
+            if (-not $IncludeUpdatesAndHotfixes.IsPresent) {
                 if ($regKeyApp.DisplayName -match '(?i)kb\d+') { continue }
                 if ($regKeyApp.DisplayName -match 'Cumulative Update') { continue }
                 if ($regKeyApp.DisplayName -match 'Security Update') { continue }
@@ -115,13 +133,13 @@ Function Get-RedstoneInstalledApplication {
             $appPublisher = $regKeyApp.Publisher -replace '[^\u001F-\u007F]',''
 
             ## Determine if application is a 64-bit application
-            [boolean]$Is64BitApp = if (([System.Environment]::Is64BitOperatingSystem) -and ($regKeyApp.PSPath -notmatch '^Microsoft\.PowerShell\.Core\\Registry::HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node')) { $true } else { $false }
+            [boolean] $Is64BitApp = if (([System.Environment]::Is64BitOperatingSystem) -and ($regKeyApp.PSPath -notmatch '^Microsoft\.PowerShell\.Core\\Registry::HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node')) { $true } else { $false }
 
-            if ($ProductCode) {
+            if ($PSCmdlet.ParameterSetName -eq 'ProductCode') {
                 ## Verify if there is a match with the product code passed to the script
-                if ($regKeyApp.PSChildName -match [regex]::Escape($productCode)) {
+                if (($regKeyApp.PSChildName -as [guid]).Guid -eq ($ProductCode -as [guid]).Guid) {
                     Write-Information "[Get-RedstoneInstalledApplication] Found installed application [$appDisplayName] version [$appDisplayVersion] matching product code [$productCode]."
-                    $installedApplication += New-Object -TypeName 'PSObject' -Property @{
+                    $installedApplication += [PSObject] @{
                         UninstallSubkey = $regKeyApp.PSChildName
                         ProductCode = $regKeyApp.PSChildName -as [guid]
                         DisplayName = $appDisplayName
@@ -136,35 +154,55 @@ Function Get-RedstoneInstalledApplication {
                         PSPath = $regKeyApp.PSPath
                     }
                 }
-            }
-
-            if ($name) {
+            } else {
                 ## Verify if there is a match with the application name(s) passed to the script
                 foreach ($application in $Name) {
                     $applicationMatched = $false
-                    if ($exact) {
-                        #  Check for an exact application name match
-                        if ($regKeyApp.DisplayName -eq $application) {
+                    if ($Exact.IsPresent) {
+                        Write-Debug ('[Get-RedstoneInstalledApplication] $Exact.IsPresent')
+                        #  Check for exact application name match
+                        if ($CaseSensitive.IsPresent) {
+                            #  Check for a CaseSensitive application name match
+                            if ($regKeyApp.DisplayName -ceq $application) {
+                                $applicationMatched = $true
+                                Write-Information "[Get-RedstoneInstalledApplication] Found installed application [$appDisplayName] version [$appDisplayVersion] using casesensitive exact name matching for search term [$application]."
+                            }
+                        } elseif ($regKeyApp.DisplayName -eq $application) {
                             $applicationMatched = $true
                             Write-Information "[Get-RedstoneInstalledApplication] Found installed application [$appDisplayName] version [$appDisplayVersion] using exact name matching for search term [$application]."
                         }
-                    }
-                    elseif ($WildCard.IsPresent) {
-                        #  Check for wildcard application name match
-                        if ($regKeyApp.DisplayName -like $application) {
+                    } elseif ($RegEx.IsPresent) {
+                        Write-Debug ('[Get-RedstoneInstalledApplication] $RegEx.IsPresent')
+                        #  Check for a regex application name match
+                        if ($CaseSensitive.IsPresent) {
+                            #  Check for a CaseSensitive application name match
+                            if ($regKeyApp.DisplayName -cmatch $application) {
+                                $applicationMatched = $true
+                                Write-Information "[Get-RedstoneInstalledApplication] Found installed application [$appDisplayName] version [$appDisplayVersion] using casesensitive regex name matching for search term [$application]."
+                            }
+                        } elseif ($regKeyApp.DisplayName -match $application) {
                             $applicationMatched = $true
-                            Write-Information "[Get-RedstoneInstalledApplication] Found installed application [$appDisplayName] version [$appDisplayVersion] using wildcard matching for search term [$application]."
+                            Write-Information "[Get-RedstoneInstalledApplication] Found installed application [$appDisplayName] version [$appDisplayVersion] using regex name matching for search term [$application]."
                         }
-                    }
-                    #  Check for a regex application name match
-                    elseif ($regKeyApp.DisplayName -match [regex]::Escape($application)) {
-                        $applicationMatched = $true
-                        Write-Information "[Get-RedstoneInstalledApplication] Found installed application [$appDisplayName] version [$appDisplayVersion] using regex matching for search term [$application]."
+                    } else {
+                        #  Check for a like application name match
+                        if ($CaseSensitive.IsPresent) {
+                            #  Check for a CaseSensitive application name match
+                            if ($regKeyApp.DisplayName -clike $application) {
+                                $applicationMatched = $true
+                                Write-Information "[Get-RedstoneInstalledApplication] Found installed application [$appDisplayName] version [$appDisplayVersion] using casesensitive like name matching for search term [$application]."
+                            } else {
+                                Write-Information "[Get-RedstoneInstalledApplication] No found installed application using casesensitive like name matching for search term [$application]."
+                            }
+                        } elseif ($regKeyApp.DisplayName -like $application) {
+                            $applicationMatched = $true
+                            Write-Information "[Get-RedstoneInstalledApplication] Found installed application [$appDisplayName] version [$appDisplayVersion] using like name matching for search term [$application]."
+                        }
                     }
 
                     if ($applicationMatched) {
                         # $installedApplication += $regKeyApp
-                        $installedApplication += New-Object -TypeName 'PSObject' -Property @{
+                        $installedApplication += [PSObject] @{
                             UninstallSubkey = $regKeyApp.PSChildName
                             ProductCode = $regKeyApp.PSChildName -as [guid]
                             DisplayName = $appDisplayName
@@ -182,11 +220,12 @@ Function Get-RedstoneInstalledApplication {
                 }
             }
         } catch {
-            Write-Error "[Get-RedstoneInstalledApplication] Failed to resolve application details from registry for [$appDisplayName]. `n$(Resolve-Error)"
+            Write-Error "[Get-RedstoneInstalledApplication] Failed to resolve application details from registry for [$appDisplayName].$(if (Get-Command 'Resolve-Error' -ErrorAction 'Ignore') { "`n{0}" -f (Resolve-Error) })"
             continue
         }
     }
 
+    Write-Information ('[Get-RedstoneInstalledApplication] Application Searched: {0}' -f $application)
     return $installedApplication
 }
 #region DEVONLY
